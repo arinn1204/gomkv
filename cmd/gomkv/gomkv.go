24dc274ea6b158d6e34b5cd36e6a89cda0a836bd
arinn <arinn1250@gmail.com>
2022-05-21 17:28:29 -0700

    added basic printing of cli args

--- cmd/gomkv/gomkv.go
+++ cmd/gomkv/gomkv.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"flag"
+	"fmt"
 )
 
 func main() {
-	path := flag.String("path", "", "The path of the file to read")
+	pathPtr := flag.String("path", " ", "The path of the file to read")
+	flag.Parse()
 
+	fmt.Printf("Path to read: '%v'\n", *pathPtr)
 }
