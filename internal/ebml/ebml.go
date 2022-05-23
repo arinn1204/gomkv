f602c545ac7a52f85a7d6698cb7068f8ef78b0bd
arinn <arinn1250@gmail.com>
2022-05-22 17:42:46 -0700

    added default return

--- internal/ebml/ebml.go
+++ internal/ebml/ebml.go
@@ -1,13 +1,13 @@
 package ebml
 
 import (
-	"github.com/arinn1204/gomkv/internal/ebml/types/document"
+	"github.com/arinn1204/gomkv/internal/ebml/types"
 	"github.com/arinn1204/gomkv/internal/io"
 )
 
 //EbmlReader is the interface that will parse the file to obtain the metadata
 type EbmlReader interface {
-	read(*io.File) document.EbmlDocument
+	read(*io.File) types.EbmlDocument
 }
 
 //Ebml is the struct used to receive the interface
@@ -19,6 +19,6 @@ func init() {
 	ebml = Ebml{}
 }
 
-func (ebml Ebml) Read(f *io.File) document.EbmlDocument {
-
+func (ebml Ebml) Read(f *io.File) types.EbmlDocument {
+	return types.EbmlDocument{}
 }
