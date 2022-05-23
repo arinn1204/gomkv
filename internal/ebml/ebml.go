06f65031cf9c9b4421045321d51a4e5a59cbe76d
arinn <arinn1250@gmail.com>
2022-05-22 17:18:49 -0700

    added initial structure

--- internal/ebml/ebml.go
+++ internal/ebml/ebml.go
@@ -1,1 +1,24 @@
 package ebml
+
+import (
+	"github.com/arinn1204/gomkv/internal/ebml/types/document"
+	"github.com/arinn1204/gomkv/internal/io"
+)
+
+//EbmlReader is the interface that will parse the file to obtain the metadata
+type EbmlReader interface {
+	read(*io.File) document.EbmlDocument
+}
+
+//Ebml is the struct used to receive the interface
+type Ebml struct{}
+
+var ebml Ebml
+
+func init() {
+	ebml = Ebml{}
+}
+
+func (ebml Ebml) Read(f *io.File) document.EbmlDocument {
+
+}
