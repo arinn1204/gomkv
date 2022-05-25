75bdbd5422cddacca4d0bd2affc6a99c37e628ba
arinn <arinn1250@gmail.com>
2022-05-25 05:42:53 -0700

    exporting the file pointer

--- internal/io/file.go
+++ internal/io/file.go
@@ -5,12 +5,12 @@ import "os"
 //File is the structure that will be used to mock the file pointer
 //Eventually this will be used to have a mutex to control multiple threads
 type File struct {
-	file *os.File
+	File *os.File
 }
 
 //Read is a wrapper around os.File.Read
 func Read(f *File, startPos uint, buf []byte) int {
-	file := f.file
+	file := f.File
 
 	file.Seek(int64(startPos), 0)
 
