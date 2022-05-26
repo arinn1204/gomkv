315ee0363138a5fdae5965568450a2f26acd0f1e
arinn <arinn1250@gmail.com>
2022-05-25 20:14:58 -0700

    added a dummy implementation

--- cmd/gomkv/main.go
+++ cmd/gomkv/main.go
@@ -1,13 +1,26 @@
 package main
 
 import (
-	"flag"
-	"fmt"
+	"os"
+
+	"github.com/arinn1204/gomkv/internal/ebml"
+	"github.com/arinn1204/gomkv/internal/io"
 )
 
 func main() {
-	pathPtr := flag.String("path", " ", "The path of the file to read")
-	flag.Parse()
+	filePath := "../../data/Better.Call.Saul.S06.SPECIAL.American.Greed.James.McGill.1080p.AMZN.WEB-DL.DDP2.0.H.264-SKiZOiD.mkv"
+	file, err := os.Open(filePath)
 
-	fmt.Printf("Path to read: '%v'\n", *pathPtr)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	ebml := ebml.Ebml{}
+
+	container := io.File{
+		File: file,
+	}
+
+	ebml.Read(&container)
 }
