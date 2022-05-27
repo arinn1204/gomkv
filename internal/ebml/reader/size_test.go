87e294bd92339b2d3957368b963f9876f220a947
arinn <arinn1250@gmail.com>
2022-05-26 21:27:07 -0700

    setup test to do size calculations

--- internal/ebml/reader/size_test.go
+++ internal/ebml/reader/size_test.go
@@ -10,49 +10,80 @@ import (
 )
 
 type testData struct {
-	size      int64
-	readCount int
-	numCalls  int
+	size     int64
+	numCalls int
 }
 
 func TestGetSizeWithDifferentWidths(t *testing.T) {
 	sizes := []testData{
 		{
-			size:      1,
-			readCount: 1,
-			numCalls:  2,
+			size:     127,
+			numCalls: 1,
+		},
+		{
+			size:     16147,
+			numCalls: 2,
+		},
+		{
+			size:     2036498,
+			numCalls: 2,
+		},
+		{
+			size:     252908049,
+			numCalls: 2,
+		},
+		{
+			size:     30384722192,
+			numCalls: 2,
+		},
+		{
+			size:     3380442370063,
+			numCalls: 2,
+		},
+		{
+			size:     865393246736142,
+			numCalls: 2,
+		},
+		{
+			size:     5367889050668557,
+			numCalls: 3,
 		},
 	}
 
-	data := []byte{
-		byte(6),
-		byte(2),
-		byte(3),
-		byte(4),
-		byte(5),
-		byte(6),
-		byte(7),
-	}
-
 	for i, expected := range sizes {
-		testName := fmt.Sprintf("GetSize(width=%v)", i)
 		ebml := &mocks.Reader{}
 		reader := EbmlReader{
 			Reader:  ebml,
 			CurrPos: 0,
 		}
+		width, _ := widthMap.GetInverse(i + 1)
 
-		ebml.On("Read", mock.MatchedBy(expected.readCount), mock.Anything).
+		data := []byte{
+			byte(width.(int)),
+			byte(19),
+			byte(18),
+			byte(17),
+			byte(16),
+			byte(15),
+			byte(14),
+			byte(13),
+			byte(12),
+		}
+
+		alreadyRead := 0
+
+		ebml.On("Read", mock.AnythingOfType("uint"), mock.Anything).
 			Return(1).
 			Run(func(args mock.Arguments) {
 				count := args.Get(0).(int)
 				arr := args.Get(1).([]byte)
 				//mimic the reading of data and copy into the argument array
-				copy(arr, data[0:count])
+				copy(arr, data[alreadyRead:count])
+
+				alreadyRead += count
 			})
 
-		width, _ := widthMap.GetInverse(i)
-		data[0] = byte(width.(int))
+		testName := fmt.Sprintf("GetSize(width=%v)", i)
 
 		t.Run(
 			testName,
