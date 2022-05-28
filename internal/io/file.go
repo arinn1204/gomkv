31865c86fcc47302731fb4f4aba0eaff5cdd76b7
arinn <arinn1250@gmail.com>
2022-05-27 21:33:13 -0700

    added mutex around the read operations

--- internal/io/file.go
+++ internal/io/file.go
@@ -1,17 +1,23 @@
 package io
 
-import "os"
+import (
+	"os"
+	"sync"
+)
 
 //File is the structure that will be used to mock the file pointer
 //Eventually this will be used to have a mutex to control multiple threads
 type File struct {
-	File *os.File
+	File  *os.File
+	Mutex sync.Mutex
 }
 
 var ebmlFile File
 
 //Read is a wrapper around os.File.Read
 func (ebmlFile *File) Read(startPos uint, buf []byte) int {
+	ebmlFile.Mutex.Lock()
+	defer ebmlFile.Mutex.Unlock()
 	file := ebmlFile.File
 
 	file.Seek(int64(startPos), 0)
