6da724809b219045b2e478ad61958a80f91cdf32
arinn <arinn1250@gmail.com>
2022-05-29 21:22:41 -0700

    initial commit

---
+++ internal/ebml/segment.go
@@ -0,0 +1,12 @@
+package ebml
+
+import (
+	"github.com/arinn1204/gomkv/internal/ebml/specification"
+	"github.com/arinn1204/gomkv/pkg/types"
+)
+
+func createSegment(ebml Ebml, spec specification.Ebml) (types.Segment, error) {
+	segment := types.Segment{}
+
+	return segment, nil
+}
