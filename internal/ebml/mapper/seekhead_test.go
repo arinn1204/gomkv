d081edf7c039e86127d93062d283d2dd138c3520
arinn <arinn1250@gmail.com>
2022-06-05 12:45:56 -0700

    refactored tests to monkey patch buffer operations

--- internal/ebml/mapper/seekhead_test.go
+++ internal/ebml/mapper/seekhead_test.go
@@ -1,18 +1,56 @@
 package mapper
 
 import (
+	"encoding/binary"
 	"testing"
 
+	"github.com/arinn1204/gomkv/internal/ebml"
 	"github.com/arinn1204/gomkv/pkg/types"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestWillProperlySerializeASeekHead(t *testing.T) {
-	seekHeadData := []byte{77, 187, 140, 83, 171, 132, 21, 73, 169, 102, 83, 172, 130, 16, 3, 77, 187, 140, 83, 171, 132, 22, 84, 174, 107, 83, 172, 130, 16, 131, 77, 187, 142, 83, 171, 132, 28, 83, 187, 107, 83, 172, 132, 46, 16, 255, 240, 77, 187, 142, 83, 171, 132, 18, 84, 195, 103, 83, 172, 132, 46, 17, 65, 154, 236, 79}
+	//seekHeadData := []byte{77, 187, 140, 83, 171, 132, 21, 73, 169, 102, 83, 172, 130, 16, 3, 77, 187, 140, 83, 171, 132, 22, 84, 174, 107, 83, 172, 130, 16, 131, 77, 187, 142, 83, 171, 132, 28, 83, 187, 107, 83, 172, 132, 46, 16, 255, 240, 77, 187, 142, 83, 171, 132, 18, 84, 195, 103, 83, 172, 132, 46, 17, 65, 154, 236, 79}
+	idCount := 0
+	GetID = func(ebml *ebml.Ebml, maxCount int) (uint32, error) {
+		var id uint32
+		switch idCount {
+		case 0:
+			id = 0x4DBB
+		case 1:
+			id = 0x53AC //seek ID
+		case 2:
+			id = 0x53AB //seek Position
+		}
+		idCount++
+		return id, nil
+	}
 
-	reader := getMockData(seekHeadData)
+	read = func(ebml *ebml.Ebml, data []byte) (int, error) {
+		switch idCount {
+		case 2:
+			binary.BigEndian.PutUint32(data, 0x1003)
+		case 3:
+			binary.BigEndian.PutUint64(data, 0x1549a966)
+		}
+		return len(data), nil
+	}
 
-	seekHeadRes, err := seekHead{}.Map(int64(len(seekHeadData)), *reader)
+	getSize = func(ebml *ebml.Ebml) (int64, error) {
+		var size int64
+		switch idCount {
+		case 1:
+			size = 12
+		case 2:
+			size = 4
+		case 3:
+			size = 8
+		}
+
+		return size, nil
+	}
+
+	seekHeadRes, err := seekHead{}.Map(int64(12), *testEbmlObj)
 
 	assert.Nil(t, err)
 
@@ -21,18 +59,6 @@ func TestWillProperlySerializeASeekHead(t *testing.T) {
 			{
 				SeekPosition: 0x1003,
 				SeekID:       0x1549a966,
-			},
-			{
-				SeekPosition: 0x1083,
-				SeekID:       0x1654ae6b,
-			},
-			{
-				SeekPosition: 0x2e10fff0,
-				SeekID:       0x1c53bb6b,
-			},
-			{
-				SeekPosition: 0x2e11419a,
-				SeekID:       0x1254c367,
 			},
 		},
 	}
