96d3456e86d61dcb32be53e06489fc8f27a84fa5
arinn <arinn1250@gmail.com>
2022-06-06 21:31:53 -0700

    initial commit

---
+++ internal/ebml/mapper/tags_test.go
@@ -0,0 +1,11 @@
+package mapper
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTagsTest(t *testing.T) {
+	assert.Fail(t, "Tags need to be tested")
+}
---
+++ internal/ebml/mapper/track_test.go
@@ -0,0 +1,11 @@
+package mapper
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTracksFail(t *testing.T) {
+	assert.Fail(t, "Tracks need to be tested")
+}
